dba4ee5d8ea46e8505cf0b841bfea9fa11b53eda
Connor Irwin <connorai2004@gmail.com>
2020-04-07 22:57:32 -0500

    jsnes

---
+++ precache-manifest.3d0251f5fca0c34169c52ab7ff7944a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196ccab2b504db5261983ddbeb5c1ef5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55caf7c2c7c67679cb73",
+    "url": "/static/css/main.43020cd3.chunk.css"
+  },
+  {
+    "revision": "700208df01e201161749",
+    "url": "/static/js/2.f0ffbaf4.chunk.js"
+  },
+  {
+    "revision": "ce788a1fa508dab3627efbe6528a9a6f",
+    "url": "/static/js/2.f0ffbaf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55caf7c2c7c67679cb73",
+    "url": "/static/js/main.50b5dc1b.chunk.js"
+  },
+  {
+    "revision": "dc6c9145439f3ba64f0c",
+    "url": "/static/js/runtime-main.be5a99a7.js"
+  }
+]);
